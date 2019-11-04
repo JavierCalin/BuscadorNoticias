ccfa5a0e3a7b36535cb733bc2c00811aec6a86d4
Javier.Calin <javier.calindb@gmail.com>
2019-11-04 16:28:57 +0100

    añadida llamada async a API noticias

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    noticias : []
+   }
+  componentDidMount(){
+    this.consultarApi()
+  }
+  consultarApi = async ()=>{
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f69e45d9302e447fbb91fe3a68952ec7`
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json()
+
+    this.setState({
+      noticias : noticias.articles
+    })
+  }
+
+  render() { 
+    return ( <h1>Noticias React</h1> );
+  }
 }
-
+ 
 export default App;
